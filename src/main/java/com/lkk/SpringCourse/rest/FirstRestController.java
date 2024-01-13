85c47221db8226e8adea5b3b5b0448c244ae4f19
LamiKaan <65834084+LamiKaan@users.noreply.github.com>
2024-01-13 21:54:36 +0300

    Initial commit

---
+++ src/main/java/com/lkk/SpringCourse/rest/FirstRestController.java
@@ -0,0 +1,23 @@
+package com.lkk.SpringCourse.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+    @Value("${welcome.text}")
+    private String welcomeMessage;
+
+    // Expose "/" to return "Helo World"
+    @GetMapping(path = {"/"})
+    public String printWelcomeMessage() {
+        return welcomeMessage;
+    }
+
+    // Expose a new endpoint "/friends"
+    @GetMapping(path = {"/friends"})
+    public String listFriends() {
+        return "0 friends";
+    }
+}
